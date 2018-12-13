833ce1252e9d0d38a219dcfa571e3f1a45d904a3
NobleYang <NobleYang@192.168.13.1>
2018-12-13 11:14:33 +0800

    给CustomerRespository接口的addCustomer方法加了注释

--- DemoBankCommons/src/main/java/cn/com/demo/ssm/bank/respository/IBankCustomerInfoRespository.java
+++ DemoBankCommons/src/main/java/cn/com/demo/ssm/bank/respository/IBankCustomerInfoRespository.java
@@ -5,6 +5,9 @@ import java.util.List;
 import cn.com.demo.ssm.bank.entity.BankCustomerInfo;
 
 public interface IBankCustomerInfoRespository {
+	/**
+	 * 添加一个用户信息
+	 * */
     public BankCustomerInfo addCustomerInfo(BankCustomerInfo customer);
     public List<BankCustomerInfo> selectAllCustomer();
 }
