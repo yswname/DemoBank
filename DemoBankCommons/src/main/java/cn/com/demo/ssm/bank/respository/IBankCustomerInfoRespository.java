b14d65a283a77a2e6050ad0058cd25649dcc049a
NobleYang <NobleYang@192.168.13.1>
2018-12-13 13:48:25 +0800

    ee

--- DemoBankCommons/src/main/java/cn/com/demo/ssm/bank/respository/IBankCustomerInfoRespository.java
+++ DemoBankCommons/src/main/java/cn/com/demo/ssm/bank/respository/IBankCustomerInfoRespository.java
@@ -6,7 +6,13 @@ import cn.com.demo.ssm.bank.entity.BankCustomerInfo;
 
 public interface IBankCustomerInfoRespository {// 4444用111戶444111
 	/**
-	 * 添加用户
+	 * 111111
+	 * 第四個人
+	 * 44444
+	 * 添4444加一个444用11111户444411111信息
+	 * 1111111
+	 * 第四個人
+	 * 44444
 	 * */
     public BankCustomerInfo addCustomerInfo(BankCustomerInfo customer);
     /**
