be1e0cc3e6ed275168be8299218e52c87353bd7f
NobleYang <NobleYang@192.168.13.1>
2018-12-13 11:13:09 +0800

    给CustomerInfoRespository接口的selectAllCustomer方法添加的注释

--- DemoBankCommons/src/main/java/cn/com/demo/ssm/bank/respository/IBankCustomerInfoRespository.java
+++ DemoBankCommons/src/main/java/cn/com/demo/ssm/bank/respository/IBankCustomerInfoRespository.java
@@ -6,5 +6,8 @@ import cn.com.demo.ssm.bank.entity.BankCustomerInfo;
 
 public interface IBankCustomerInfoRespository {
     public BankCustomerInfo addCustomerInfo(BankCustomerInfo customer);
+    /**
+     * 查询所有的用户列表
+     * */
     public List<BankCustomerInfo> selectAllCustomer();
 }
